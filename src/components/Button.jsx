b91fd4f194bec88d7681ed49020ecf36910c3522
Chukwunnonso <cnnonso@gmail.com>
2022-11-27 21:53:11 -0800

    changed components to class

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,17 +1,43 @@
 
+//import React from "react";
 
-const Button = ({
-    text="Click me...",
-    onClick
-}) => {
-  return (
-    <button 
-        className="bg-white py-1 px-2 rounded-lg text-lg font-bold hover:scale-110"
-        onClick={onClick}
-        >
-        {text}
-    </button>
-  )
+
+
+// const Button = ({
+//     text="Click me...",
+//     onClick
+// }) => {
+//   return (
+//     <button 
+//         className="bg-white py-1 px-2 rounded-lg text-lg font-bold hover:scale-110"
+//         onClick={onClick}
+//         >
+//         {text}
+//     </button>
+//   )
+// }
+
+// export default Button;
+
+
+
+
+
+import React from "react";
+
+
+
+class Button extends React.Component {
+  render () {
+    return (
+      <button 
+          className="bg-white py-1 px-2 rounded-lg text-lg font-bold hover:scale-110">
+      </button>
+    )
+  }
+  
 }
-
+  
+ 
+  
 export default Button;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,31 +1,82 @@
 import { useState } from "react"
 import Button from "./Button"
 
-const Card = () => {
+// functional component. A FUNCTION THAT RETURNS SOMEHING FOR US
+// const Card = () => {
 
-    const [number, setNumber] = useState(0)
+//     const [number, setNumber] = useState(0)
 
-    const countDown =()=> {
-        setNumber(number-1)
+//     const countDown =()=> {
+//         setNumber(number-1)
+//     }
+
+//     const countUp =()=> {
+//         setNumber(number+1)
+//     }
+
+// return   (
+//     <div className="bg-white w-60 h-auto rounded-xl shadow-xl">
+//         <div className="bg-blue-900 flex justify-center rounded-t-xl py-2">
+//             <h1 className="text-2xl text-slate-50">Counter App</h1>
+//         </div>
+//         <div className="py-10 flex justify-center">
+//             <h2 className="text-8xl">{number}</h2>
+//         </div>
+//         <div className="bg-blue-900 rounded-b-xl py-4 flex justify-evenly">
+//             <Button text="Reduce" onClick={countDown}/>
+//             <Button text="Add"onClick={countUp}/>
+//         </div>
+//     </div>
+//   )}
+
+// export default Card
+
+
+//CounterAppClass components
+
+import React, { Component } from 'react'
+
+export default class Card extends Component {
+
+    constructor(props) {
+        super();
+        this.state = {
+            count: 0
+        }
     }
 
-    const countUp =()=> {
-        setNumber(number+1)
+    handleIncrease() {
+        this.setState({
+            count: this.state.count + 1
+        })
     }
 
-return   (
-    <div className="bg-white w-60 h-auto rounded-xl shadow-xl">
-        <div className="bg-blue-900 flex justify-center rounded-t-xl py-2">
-            <h1 className="text-2xl text-slate-50">Counter App</h1>
-        </div>
-        <div className="py-10 flex justify-center">
-            <h2 className="text-8xl">{number}</h2>
-        </div>
-        <div className="bg-blue-900 rounded-b-xl py-4 flex justify-evenly">
-            <Button text="Reduce" onClick={countDown}/>
-            <Button text="Add"onClick={countUp}/>
-        </div>
-    </div>
-  )}
+    handleDecrease() {
+      this.setState({
+          count: this.state.count -1
+      })
+  }
 
-export default Card
+    render() {
+        return (
+            <div className='card'>
+                <div className="bg-gray-700 flex justify-center rounded-t-xl py-2 text-white">
+                    <h3 className='text-center mb-2'>Class Component Counter(3 C)</h3>
+                </div>
+
+                <div className="py-10 flex justify-center text-8xl">
+                  <h5 className="bg-white">{this.state.count}</h5>
+                </div>
+
+                <div className='bg-gray-800 rounded-b-xl py-4 flex justify-evenly text-white'>
+                  <button onClick={this.handleDecrease.bind(this)}>Decrease</button>
+                  <div className="text-bold">|</div>
+                  <button  onClick={this.handleIncrease.bind(this)}>Increase</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+
+//className='bg-blue-900 rounded-b-xl py-4 flex justify-evenly'
